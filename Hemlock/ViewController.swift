38dfc029d076966f2d68439ca73ed0a715abe4dc
Sylvan Martin <slm338@cornell.edu>
2024-07-25 23:29:27 -0400

    Initial Commit

---
+++ Hemlock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hemlock
+//
+//  Created by Sylvan Martin on 7/25/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
